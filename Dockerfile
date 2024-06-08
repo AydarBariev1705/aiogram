586a4fbd3a31abdcb3bb9d108e96a31a64f09276
Aydar Bariev <bariev@Kub.myguest.virtualbox.org>
2024-06-08 11:18:46 +0300

    Добавлено логирование в файл

--- Dockerfile
+++ Dockerfile
@@ -2,22 +2,12 @@ FROM python:3.10
 
 ENV PYTHONUNBUFFERED=1
 
-
-
+WORKDIR /bot
 COPY requirements.txt requirements.txt
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 
 
-COPY admin.py admin.py
-COPY bot.py bot.py
-COPY config.py config.py
-COPY database.py database.py
-COPY handlers.py handlers.py
-COPY keyboards.py keyboards.py
-COPY models.py models.py
-COPY payment_handler.py payment_handler.py
-COPY states.py states.py
-COPY utils.py  utils.py
+COPY bot .
 COPY .env .env
 CMD ["python","bot.py","run"]
---
+++ bot/bot.py
@@ -0,0 +1,35 @@
+import asyncio
+import logging
+
+from config import TOKEN
+from handlers import router
+from payment_handler import order, process_pre_checkout_query, successful_payment
+from aiogram import Bot, Dispatcher, F
+
+from admin import admin
+
+logging.basicConfig(format=u'%(filename)+13s [ LINE:%(lineno)-4s] %(levelname)-8s [%(asctime)s] %(message)s',
+                    level=logging.INFO, filename='logs.log')
+
+
+async def main():
+    """
+    Главная функция для запуска бота
+    """
+    bot = Bot(token=TOKEN)
+
+    dp = Dispatcher()
+    dp.include_routers(router, admin)
+    dp.callback_query.register(order, F.data == 'checkout')
+    dp.pre_checkout_query.register(process_pre_checkout_query)
+    dp.message.register(successful_payment, F.successful_payment)
+
+    await dp.start_polling(bot, skip_updates=True)
+
+
+if __name__ == "__main__":
+    try:
+        print('Start polling')
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logging.info("Bot stopped")
